2cc6f0911d7e68e08c3c2ebd8fd78d693a3e1d9f
Stephen Michaels <stephen.michaels3333@gmail.com>
2022-10-02 23:08:11 -0400

    initial commit

---
+++ dogprojectEmma/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+string dogName = "Emma";
+bool treats = true;
+int dogAge = 3;
+decimal weight = 50.5m;
+double blankets = 33;
+char nickName = 'M';
+
+Console.WriteLine($"My dog's name is {dogName}.  She is {dogAge} years old.  She has {blankets} blankets for comfort.  It is {treats} Emma loves treats, and weighs {weight} Lbs.");
+
+
+
+
+
